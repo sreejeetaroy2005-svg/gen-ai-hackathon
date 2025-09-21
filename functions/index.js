b82fd44665c1ebaf9d1f1d628d35fc34ea98595b
sreejeetaroy2005-svg <sreejeetaroy2005@gmail.com>
2025-09-21 18:52:22 +0530

    Your commit message

--- functions/index.js
+++
@@ -1,9 +0,0 @@
-
-
-const {setGlobalOptions} = require("firebase-functions");
-const {onRequest} = require("firebase-functions/https");
-const logger = require("firebase-functions/logger");
-
-
-setGlobalOptions({ maxInstances: 10 });
-
